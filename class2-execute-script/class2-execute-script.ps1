862a84cffbd6d4c17ca73113b5956158caf8eef6
Vitor Garcia <vitor.stabile@hotmail.com>
2020-12-31 19:28:26 -0300

    Push class 1 and class 2

---
+++ class2-execute-script/class2-execute-script.ps1
@@ -0,0 +1,5 @@
+PS> Set-ExecutionPolicy unrestricted
+
+PS> cd 'C:\Users\solun\OneDrive\Projetos Programação\Projetos PowerShell\powershell-basics\class1-search-by-name\'
+
+PS> .\class1-search-by-name.ps1
