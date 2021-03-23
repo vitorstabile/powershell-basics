9463696bce5cf4ad5e1d3e78f1dc1819e2eadf52
Vitor Stabile Garcia <59717848+vitorstabile@users.noreply.github.com>
2021-03-23 19:00:55 -0300

    Delete class2-execute-script directory

--- class2-execute-script/class2-execute-script.ps1
+++
@@ -1,5 +0,0 @@
-PS> Set-ExecutionPolicy unrestricted
-
-PS> cd 'C:\Users\solun\OneDrive\Projetos Programação\Projetos PowerShell\powershell-basics\class1-search-by-name\'
-
-PS> .\class1-search-by-name.ps1
